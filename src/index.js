f023a7d1ca689d3990fafa66dc557f2a12b18577
ANDRES FELIPE H <ahoyos@lean-tech.io>
2020-01-13 10:50:51 -0500

    Setting project Sass, Folders, Design framework

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './views/App/App';
 import * as serviceWorker from './serviceWorker';
+
+import './index.scss';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
